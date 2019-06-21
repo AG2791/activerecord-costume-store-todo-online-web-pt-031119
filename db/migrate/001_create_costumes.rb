22eded1091369363fd86f6b68a26045c2b139931
AJ Guiles <ashiguiles@gmail.com>
2019-06-21 03:37:08 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -21,3 +21,16 @@
 #   end
   
 # end
+
+class CreateCostumes < ActiveRecord::Migration
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :size
+      t.string :image_url
+      t.float :price
+
+      t.timestamps null: false
+    end
+  end
+end
