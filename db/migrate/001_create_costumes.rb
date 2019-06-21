d4685d0fb4e8c4e22c4ee0e9a15bf1cba9bb8b99
AJ Guiles <ashiguiles@gmail.com>
2019-06-21 03:38:34 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,19 +8,6 @@
 # In this change method, create columns with the correct names and 
 # value types according to the spec
 
-# class CreateCostumes <ActiveRecord::Migration
-#   def change
-#     create_table :costumes do |t|
-#       t.string :name
-#       t.string :size
-#       t.string :image_url
-#       t.float :price
-
-#       t.timestamps 
-#     end
-#   end
-  
-# end
 
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
